d53318e90bbb9c864b8e289b487905c00166e97b
Diego <belydiego8@gmail.com>
2022-11-25 12:40:55 -0300

    instalando-cosas

---
+++ instalando-cosas/index.ts
@@ -0,0 +1,11 @@
+const palabra: String = "hola";
+console.log(palabra);
+
+function mostrarnombre(nombre: string) {
+  console.log(nombre);
+}
+
+function main() {
+  mostrarnombre("Diego");
+}
+main();
