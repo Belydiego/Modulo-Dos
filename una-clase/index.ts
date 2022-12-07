686dea268785e6944345001e7d0266058230f8f1
Belydiego <belydiego8@gmail.com>
2022-12-07 18:01:51 -0300

    una clase index.ts

---
+++ una-clase/index.ts
@@ -0,0 +1,48 @@
+// objeto literal creado sin una clase
+/*const objeto = {
+  titulo: "titulo",
+  id: 1,
+};*/
+
+// crear la clase Banda aquí
+
+class Banda {
+  miembros: [];
+}
+
+// no modificar este test
+function testClaseBanda() {
+  const members = ["dana", "pau"];
+  const fa = {
+    title: "album 1",
+    songs: ["album 1 - tema 1", "album 1 - tema 2"],
+  };
+  const banda = new Banda(members, [
+    fa,
+    {
+      title: "album 2",
+      songs: ["album 2 - tema 1", "album 2 - tema 2", "album 2 - tema 3"],
+    },
+  ]);
+
+  const firstAlbum = banda.getFirstAlbum();
+  const allMembers = banda.getAllMembers();
+  const randomSongAlbum1 = banda.getRandomSong("album 1");
+
+  if (
+    firstAlbum.title == fa.title &&
+    fa.songs.length == firstAlbum.songs.length &&
+    JSON.stringify(allMembers.sort()) == JSON.stringify(members.sort()) &&
+    fa.songs.includes(randomSongAlbum1)
+  ) {
+    console.log("testClaseBanda passed");
+  } else {
+    throw "testClaseBanda not passed";
+  }
+}
+
+function main() {
+  testClaseBanda();
+}
+
+main();
